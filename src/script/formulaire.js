1613e03bdf89088bdd8d2825645ca00ff6fb1fdf
rekkylaws <donatienhochart2@gmail.com>
2022-07-07 11:23:00 +0200

    Version finale
    Changement de la totalité pour la version final qui était précedemment disponible sur un autre repository.

---
+++ src/script/formulaire.js
@@ -0,0 +1,9 @@
+function afficherFormulaire() {
+	let refForm = document.getElementById("contact-form");
+	refForm.style.bottom="0";
+}
+
+function cacherFormulaire () {
+	let refForm = document.getElementById("contact-form");
+	refForm.style.bottom="-100%";
+}
---
+++ src/script/menu.js
@@ -0,0 +1,9 @@
+function afficherMenu() {
+	let refMenu = document.getElementById("menu");
+	refMenu.style.right="0";
+}
+
+function cacherMenu() {
+	let refMenu = document.getElementById("menu");
+	refMenu.style.right="-100%";
+}
