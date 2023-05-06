285120d6fce9a4991818db043c543749ea710e0e
rekky1aws <killikomfr@gmail.com>
2023-05-06 02:50:28 +0200

    Implementing secret
    Added a hint when halfway in typing the code in the console.
    Added a confirm box to inform user that the secret can be dangerous for
    some people.
    Ability to disable the secret by retyping the code.
    CSS Animation for the secret (like rotations, color changes...).

--- src/script/secret.js
+++ src/script/secret.js
@@ -1,20 +1,41 @@
 const secretCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let counter = 0;
+let secretActivated = false;
+let secretActivatedOnce = false;
+let alertText;
 
 document.addEventListener('keydown', checkKey);
 
 function checkKey (evt)
 {
+	// Checking if enter was pressed
 	if (evt.keyCode == 13 && counter == secretCode.length) {
-		console.log("Secret unlocked");
-		let secretActivation = alert("Voulez-vous activer le secret (personnes photosensibles, abstenez-vous pour votre bien)");
+
+		if (secretActivated) {
+			console.log("Désactivation du Secret");
+			alertText = "Désactiver le secret ?";
+			secretActivated = false;
+		} else {
+			console.log("Activation du Secret");
+			alertText = "Voulez-vous activer le secret (personnes photosensibles, abstenez-vous pour votre bien). Vous pourrez le déastiver en retapant ce code.";
+			secretActivated = true;
+		}
+
+		let secretActivation = confirm(alertText);
 
 		document.body.classList.toggle('secret');
 	}
 
+	// Checking code validity
 	if (evt.keyCode == secretCode[counter]) {
 		counter ++;
 	} else {
 		counter = 0;
+	}
+
+	// Hinting user Halfway
+	if (counter == Math.floor(secretCode.length / 2) && !secretActivatedOnce) {
+		console.log("Il doit y avoir quelque chose a creuser avec ca...");
+		secretActivatedOnce = true;
 	}
 }
